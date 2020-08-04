6d3c80a87bba3c515c53024ac1a854a56e1ae31b
Abner Rodrigues <abnerluis1001@gmail.com>
2020-08-04 19:13:54 -0300

    feat: added get route for classes

--- src/controllers/ClassesController.ts
+++ src/controllers/ClassesController.ts
@@ -10,6 +10,36 @@ interface IScheduleItem {
 }
 
 export default class ClassesController {
+  async index(request: Request, response: Response) {
+    const filters = request.query;
+
+    const week_day = filters.week_day as string;
+    const subject = filters.subject as string;
+    const time = filters.time as string;
+
+    if (!week_day || !subject || !time)
+      response
+        .status(400)
+        .json({ error: 'Missing filters required on query.' });
+
+    const timeInMinutes = convertHoursToMinutes(filters.time as string);
+
+    const classes = await db('classes')
+      .whereExists(function () {
+        this.select('class_schedule.*')
+          .from('class_schedule')
+          .whereRaw('`class_schedule`.`class_id` = `classes`.`id`')
+          .whereRaw('`class_schedule`.`week_day` = ??', [Number(week_day)])
+          .whereRaw('`class_schedule`.`from` <= ??', [timeInMinutes])
+          .whereRaw('`class_schedule`.`from` > ??', [timeInMinutes]);
+      })
+      .where('classes.subject', '=', subject)
+      .join('users', 'classes.user_id', '=', 'users.id')
+      .select(['classes.*', 'users.*']);
+
+    return response.json(classes);
+  }
+
   async create(request: Request, response: Response) {
     const {
       name,
