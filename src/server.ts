6bb768b4d123598fe551c1a457ee70de9e42361b
Abner Rodrigues <abnerluis1001@gmail.com>
2020-08-04 18:37:33 -0300

    feat: added post route for classes

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 
-const app = express();
-app.use(express.json());
+import routes from './routes';
 
-app.get('/', (request, response) => {
-  response.json({
-    message: 'Hello, world!',
-  });
-});
+const app = express();
+
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
